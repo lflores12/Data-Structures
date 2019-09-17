3caeeab3b4364a067be3f68cab2debd547e1a362
Leonel <leonelaf12@gmail.com>
2019-09-17 19:09:57 -0400

    finished the cache project

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,25 +1,37 @@
-class LRUCache:
-  """
-  Our LRUCache class keeps track of the max number of nodes it
-  can hold, the current number of nodes it is holding, a doubly-
-  linked list that holds the key-value entries in the correct 
-  order, as well as a storage dict that provides fast access
-  to every node stored in the cache.
-  """
-  def __init__(self, limit=10):
-    pass
+from doubly_linked_list import DoublyLinkedList
 
-  """
+
+class LRUCache:
+    """
+    Our LRUCache class keeps track of the max number of nodes it
+    can hold, the current number of nodes it is holding, a doubly-
+    linked list that holds the key-value entries in the correct 
+    order, as well as a storage dict that provides fast access
+    to every node stored in the cache.
+    """
+
+    def __init__(self, limit=10):
+        self.limit = limit
+        self.size = 0
+        self.cache = DoublyLinkedList()
+        self.storage = dict()
+
+    """
   Retrieves the value associated with the given key. Also
   needs to move the key-value pair to the end of the order
   such that the pair is considered most-recently used.
   Returns the value associated with the key or None if the
   key-value pair doesn't exist in the cache. 
   """
-  def get(self, key):
-    pass
 
-  """
+    def get(self, key):
+        if self.size is 0 or key not in self.storage:
+            return None
+        else:
+            value = self.storage[key]
+            self.cache.move_to_front(value[1])
+            return value[0]
+    """
   Adds the given key-value pair to the cache. The newly-
   added pair should be considered the most-recently used
   entry in the cache. If the cache is already at max capacity
@@ -29,5 +41,34 @@ class LRUCache:
   want to overwrite the old value associated with the key with
   the newly-specified value. 
   """
-  def set(self, key, value):
-    pass
+
+    def set(self, key, value):
+        # self.cache.add_to_head([key, value])
+        # self.size += 1
+        # print(self.cache.head.value)
+        # self.storage[key] = value
+        # print(f'***{self.storage}')
+        # print(f'******{self.storage[key]}')
+        if key in self.storage:
+            stored_node = self.storage[key]
+            self.cache.move_to_front(stored_node[1])
+            self.storage[key] = [value, self.cache.head]
+            return
+        elif self.size == self.limit:
+            tail = self.cache.tail
+            self.cache.remove_from_tail()
+            self.cache.add_to_head([key, value])
+            self.storage[key] = [value, self.cache.head]
+            del self.storage[tail.value[0]]
+        else:
+            self.cache.add_to_head([key, value])
+            self.storage[key] = [value, self.cache.head]
+            self.size += 1
+
+
+test = LRUCache(3)
+test.set('item1', 'a')
+test.set('item2', 'b')
+test.set('item1', 'c')
+print(test.get('item1'))
+print(test.get('item3'))
